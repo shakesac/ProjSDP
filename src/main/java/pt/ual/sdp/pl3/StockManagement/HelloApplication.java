741724dc5d912e7f9ea3845809a19c9146fdec4d
shakesac <miguel@drivewide.pt>
2020-12-27 11:06:37 +0000

    Initial commit

---
+++ src/main/java/pt/ual/sdp/pl3/StockManagement/HelloApplication.java
@@ -0,0 +1,9 @@
+package pt.ual.sdp.pl3.StockManagement;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/pt/ual/sdp/pl3/StockManagement/HelloResource.java
@@ -0,0 +1,14 @@
+package pt.ual.sdp.pl3.StockManagement;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
